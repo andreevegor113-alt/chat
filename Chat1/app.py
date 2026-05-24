2d399542e9a01134fb4cbff47993b6071febd625
andreevegor113-alt <andreevegor113@gmail.com>
2026-05-24 20:52:32 +0400

    chat

--- Chat1/app.py
+++ Chat1/app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, request, session, redirect, url_for
 from flask_socketio import SocketIO, emit, join_room, leave_room
 from datetime import datetime
@@ -77,5 +79,6 @@ def handle_message(data):
 def handle_disconnect():
     pass
 
-#if __name__ == '__main__':
-#    socketio.run(app, debug=True, port=5000, allow_unsafe_werkzeug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port, log_output=None, use_reloader=None, allow_unsafe_werkzeug=True)
